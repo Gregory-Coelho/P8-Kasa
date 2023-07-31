ae41d3237e63f790cc13af97e6a1b682446f5073
Gregory <95241798+Gregory-Coelho@users.noreply.github.com>
2023-07-31 09:19:52 +0200

    wip

--- kasa/src/components/Banner.jsx
+++ kasa/src/components/Banner.jsx
@@ -16,7 +16,7 @@ export const Banner = () => {
     <div
       className={
         aboutPage
-          ? "bg-about rounded-3xl h-40"
+          ? "bg-about bg-center bg-cover rounded-3xl h-48"
           : "bg-home bg-center bg-cover rounded-3xl h-48 flex justify-center items-center text-white text-6xl font-light mx-auto"
       }
     >
