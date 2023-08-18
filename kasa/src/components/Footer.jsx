c8092be8de20eebe8ae53a7fdc27443b05c0903f
Gregory <95241798+Gregory-Coelho@users.noreply.github.com>
2023-08-18 10:17:17 +0200

    error page

--- kasa/src/components/Footer.jsx
+++ kasa/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logoWhite.svg";
 
 export const Footer = () => {
   return (
-    <div className="bg-black flex items-center flex-col w-screen justify-center">
+    <div className="bg-black flex items-center flex-col w-screen justify-center mt-5">
       <div className="mt-10 mb-5 flex items-center flex-col justify-center gap-5">
         <img src={logo} alt="logo" />
         <p className="text-white">© 2020 Kasa. All rights reserved</p>
