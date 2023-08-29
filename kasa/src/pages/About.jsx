5e4793b67049f83b86f82bc4e0b2cd9cdd386873
Gregory <95241798+Gregory-Coelho@users.noreply.github.com>
2023-08-29 17:48:42 +0200

    wip about

--- kasa/src/pages/About.jsx
+++ kasa/src/pages/About.jsx
@@ -4,20 +4,48 @@ import AboutImage from "../assets/about.png";
 import { Collapse } from "../components/Collapse";
 
 export const About = () => {
+  const aboutDetails = [
+    {
+      id: 0,
+      title: "Fiabilité",
+      text: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
+    },
+    {
+      id: 1,
+      title: "Respect",
+      text: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      id: 2,
+      title: "Service",
+      text: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+      id: 3,
+      title: "Sécurité",
+      text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ];
   return (
     <div>
       <HeaderAndFooter
         children={
-          <Banner image={AboutImage}>
-            <div className="flex justify-between gap-20">
-              <Collapse title="Description" />
-              <Collapse title="Équipements" />
-              <Collapse title="Équipements" />
-              <Collapse title="Équipements" />
+          <>
+            <Banner image={AboutImage} />
+            <div className="flex flex-col justify-between gap-5 my-10 mx-20 font-medium ">
+              {aboutDetails.map((item) => {
+                return (
+                  <Collapse
+                    key={item.id}
+                    title={item.title}
+                    content={item.text}
+                  />
+                );
+              })}
             </div>
-          </Banner>
+          </>
         }
-      ></HeaderAndFooter>
+      />
     </div>
   );
 };
