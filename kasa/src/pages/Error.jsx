91498535fa94271f8c6d48399b915eaf3b616824
Gregory <95241798+Gregory-Coelho@users.noreply.github.com>
2023-08-30 17:21:20 +0200

    wip svg error

--- kasa/src/pages/Error.jsx
+++ kasa/src/pages/Error.jsx
@@ -1,15 +1,17 @@
 import { HeaderAndFooter } from "../components/HeaderAndFooter";
 import { NavLink } from "react-router-dom";
 import error from "../assets/404.svg";
+import pageDoesntExist from "../assets/pageDoesntExist.svg";
 
 export const Error = () => {
   return (
     <HeaderAndFooter>
       <div className="flex flex-col justify-between items-center gap-14">
-        <img src={error} alt="logo" />
-        <h2 className="text-primary text-4xl/[51px] font-medium">
-          Oups! La page que vous demandez n'existe pas.
-        </h2>
+        <img src={error} alt="error" />
+        <img
+          src={pageDoesntExist}
+          alt="Oups! La page que vous demandez n'existe pas."
+        />
         <NavLink to="/" className="text-primary underline">
           Retourner sur la page d'accueil
         </NavLink>
