6b7b3832b4b1041685382fb255f0d4226b46dfb5
Gregory <95241798+Gregory-Coelho@users.noreply.github.com>
2023-11-23 19:48:35 +0100

    start error, mobile card

--- kasa/src/pages/Error.jsx
+++ kasa/src/pages/Error.jsx
@@ -8,7 +8,7 @@ export const Error = () => {
   return (
     <HeaderAndFooter>
       <div className={styles.errorCard}>
-        <img src={error} alt="error" className="errorImage" />
+        <img src={error} alt="error" className={styles.errorImage} />
         <p className={styles.errorText}>
           Oups! La page que vous demandez n'existe pas.
         </p>
