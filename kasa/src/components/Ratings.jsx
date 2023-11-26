86a9ca2742c561b68914a3ee78eeb4ed2140f663
Gregory <95241798+Gregory-Coelho@users.noreply.github.com>
2023-11-26 14:52:40 +0100

    final product

--- kasa/src/components/Ratings.jsx
+++ kasa/src/components/Ratings.jsx
@@ -6,11 +6,11 @@ import styles from '../styles/Ratings.module.css'
 export const Ratings = ({ rating }) => {
   return (
     <div className={styles.ratingsCard}>
-      <img src={rating >= 1 ? FullStar : EmptyStar} alt="Star" />
-      <img src={rating >= 2 ? FullStar : EmptyStar} alt="Star" />
-      <img src={rating >= 3 ? FullStar : EmptyStar} alt="Star" />
-      <img src={rating >= 4 ? FullStar : EmptyStar} alt="Star" />
-      <img src={rating >= 5 ? FullStar : EmptyStar} alt="Star" />
+      <img className={styles.star} src={rating >= 1 ? FullStar : EmptyStar} alt="Star" />
+      <img className={styles.star} src={rating >= 2 ? FullStar : EmptyStar} alt="Star" />
+      <img className={styles.star} src={rating >= 3 ? FullStar : EmptyStar} alt="Star" />
+      <img className={styles.star} src={rating >= 4 ? FullStar : EmptyStar} alt="Star" />
+      <img className={styles.star} src={rating >= 5 ? FullStar : EmptyStar} alt="Star" />
     </div>
   );
 };
